c8ba29fa15b441426bdfc80217440ea941b5e334
宋翔 <littlexiang521@gmail.com>
2018-03-13 01:03:19 +0800

    Update install-composer.sh

--- install-composer.sh
+++ install-composer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+/usr/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
